c2fc7b3d60244446eb598c4ddeef6e9aa7d15478
Sonia Rodriguez <sonferrod@gmail.com>
2018-01-14 13:52:40 +0000

    lesson1-java: "Hidden" the conversion between Java and Scala types for the not implemented method in FileReaderImpl

--- src/main/scala/hotels/service/FileReaderImpl.scala
+++ src/main/scala/hotels/service/FileReaderImpl.scala
@@ -1,6 +1,13 @@
 package hotels.service
 import java.util
+import scala.collection.JavaConverters._
 
 class FileReaderImpl extends FileReader {
-  override def readFile(sourcePath: String): util.List[String] = ???
+  override def readFile(sourcePath: String): util.List[String] = {
+    readFileInScala(sourcePath).asJava
+  }
+
+  private def readFileInScala(sourcePath: String): List[String] = {
+    ???
+  }
 }
