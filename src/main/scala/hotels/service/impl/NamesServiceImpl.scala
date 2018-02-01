163e3926c1d9795595f853414bc6a5bd3470a721
plape <lape.povilas@gmail.com>
2018-02-01 13:06:47 +0000

    Added hints

--- src/main/scala/hotels/service/impl/NamesServiceImpl.scala
+++ src/main/scala/hotels/service/impl/NamesServiceImpl.scala
@@ -3,16 +3,17 @@ package hotels.service.impl
 
 class NamesServiceImpl {
 
+  //find length of each word (Hint: use map)
   def findLengthOfEachName(list: List[String]): List[Int] = {
     ???
   }
 
-  //find all names which start with particular letter
+  //find all names which start with particular letter (Hint: use filter)
   def findAllNamesWithFirstLetter(list: List[String], firstLetter: String): List[String] = {
     ???
   }
 
-  //find total number of characters in the list of names
+  //find total number of characters in the list of names (Hint: use foldLeft)
   def findTotalLength(list: List[String]): Int = {
     ???
   }
