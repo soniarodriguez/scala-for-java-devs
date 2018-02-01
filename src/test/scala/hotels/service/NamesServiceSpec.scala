7250b04ba984f18d42e13c1fac14872b9118e7c9
plape <lape.povilas@gmail.com>
2018-02-01 13:05:31 +0000

    Added tests

--- src/test/scala/hotels/service/NamesServiceSpec.scala
+++ src/test/scala/hotels/service/NamesServiceSpec.scala
@@ -8,7 +8,6 @@ class NamesServiceSpec extends FlatSpec with Matchers {
   private val guestNames = List("Sonia", "Greg", "Povilas", "Angela", "John", "Adam", "Avinash", "Laura")
   private val namesService = new NamesServiceImpl
 
-
   it should "return the length of each name" in {
     val lengthList = namesService.findLengthOfEachName(guestNames)
 
@@ -21,10 +20,28 @@ class NamesServiceSpec extends FlatSpec with Matchers {
     filteredNames shouldBe List("Angela", "Adam", "Avinash")
   }
 
+  it should "return empty list if input list is empty" in {
+    val filteredNames = namesService.findAllNamesWithFirstLetter(List.empty[String], "A")
+
+    filteredNames shouldBe List.empty[String]
+  }
+
+  it should "return empty list if there are no names starting with particular letter" in {
+    val filteredNames = namesService.findAllNamesWithFirstLetter(List.empty[String], "Z")
+
+    filteredNames shouldBe List.empty[String]
+  }
+
   it should "return total number of characters in the list" in {
     val totalLen = namesService.findTotalLength(guestNames)
 
     totalLen shouldBe 42
+  }
+
+  it should "return 0 if the input list is empty" in {
+    val totalLen = namesService.findTotalLength(List.empty[String])
+
+    totalLen shouldBe 0
   }
 
 }
