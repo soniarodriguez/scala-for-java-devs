43043e6aa64cd229db4a54b433016db5aa14fca7
plape <lape.povilas@gmail.com>
2018-02-01 13:03:58 +0000

    Added tests

--- src/main/scala/hotels/service/impl/NamesServiceImpl.scala
+++ src/main/scala/hotels/service/impl/NamesServiceImpl.scala
@@ -3,18 +3,14 @@ package hotels.service.impl
 
 class NamesServiceImpl {
 
-  private val guestNames = List("Sonia", "Greg", "Povilas", "Angela", "John", "Adam", "Avinash", "Laura")
-
   def findLengthOfEachName(list: List[String]): List[Int] = {
     list.map(name => name.length)
   }
 
-  //find all names which start with particular letter
   def findAllNamesWithFirstLetter(list: List[String], firstLetter: String): List[String] = {
     list.filter(_.startsWith(firstLetter))
   }
 
-  //find total number of characters in the list of names
   def findTotalLength(list: List[String]): Int = {
     list.foldLeft(0) { (acc, name) => acc + name.length }
   }
--- src/test/scala/hotels/service/NamesServiceSpec.scala
+++ src/test/scala/hotels/service/NamesServiceSpec.scala
@@ -15,10 +15,28 @@ class NamesServiceSpec extends FlatSpec with Matchers {
     lengthList shouldBe List(5, 4, 7, 6, 4, 4, 7, 5)
   }
 
+  it should "return empty list if input list is empty" in {
+    val filteredNames = namesService.findAllNamesWithFirstLetter(List.empty[String], "A")
+
+    filteredNames shouldBe List.empty[String]
+  }
+
+  it should "return empty list if there are no names starting with particular letter" in {
+    val filteredNames = namesService.findAllNamesWithFirstLetter(List.empty[String], "Z")
+
+    filteredNames shouldBe List.empty[String]
+  }
+
   it should "return all names which start with letter A" in {
     val filteredNames = namesService.findAllNamesWithFirstLetter(guestNames, "A")
 
     filteredNames shouldBe List("Angela", "Adam", "Avinash")
+  }
+
+  it should "return 0 if the input list is empty" in {
+    val totalLen = namesService.findTotalLength(List.empty[String])
+
+    totalLen shouldBe 0
   }
 
   it should "return total number of characters in the list" in {
