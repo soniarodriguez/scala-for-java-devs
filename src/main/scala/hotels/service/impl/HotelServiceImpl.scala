5cc99c5f36c40de7c378d13e9d955d159e11c9af
Sonia Fernandez <sfernandezrodr@hotels.com>
2018-02-14 18:05:14 +0000

    Lesson4

--- src/main/scala/hotels/service/impl/HotelServiceImpl.scala
+++ src/main/scala/hotels/service/impl/HotelServiceImpl.scala
@@ -5,32 +5,12 @@ import hotels.service.HotelService
 
 class HotelServiceImpl extends HotelService {
   
-  override def getHotelNames(hotels: List[Hotel]): List[String] =
-    hotels.map(hotel => hotel.name)
+  override def getHotelNames(hotels: List[Hotel]): List[String] = ???
 
-  override def findHotelById(hotels: List[Hotel], id: Int): Option[Hotel] =
-    hotels.find(hotel => hotel.id == id)
+  override def findHotelById(hotels: List[Hotel], id: Int): Option[Hotel] = ???
 
-  override def findHotelsByCity(hotels: List[Hotel], city: String): List[Hotel] =
-    hotels.filter(hotel => hotel.city == city)
+  override def findHotelsByCity(hotels: List[Hotel], city: String): List[Hotel] = ???
 
-  override def findCheapestHotel(hotels: List[Hotel]): Hotel = {
-    hotels.reduceLeft { (cheapestHotel, hotel) =>
-      if (cheapestHotel.price <= hotel.price)
-        cheapestHotel
-      else
-        hotel
-    }
-  }
+  override def findCheapestHotel(hotels: List[Hotel]): Hotel = ???
 
-  // When hotels list can be empty
-  //  def findCheapestHotel(hotels: List[Hotel]): Option[Hotel] = {
-  //
-  //    hotels.foldLeft(Option.empty[Hotel]) { (maybeCheapestHotel, hotel) =>
-  //      if (maybeCheapestHotel.isDefined && maybeCheapestHotel.get.price <= hotel.price)
-  //        maybeCheapestHotel
-  //      else
-  //        Some(hotel)
-  //    }
-  //  }
 }
